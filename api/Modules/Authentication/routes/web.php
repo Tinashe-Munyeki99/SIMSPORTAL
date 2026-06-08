23db628539a2ab5ed369e85875fa50b142f79eaf
sbldevops <gitlab@simbisabrands.com>
2026-06-08 14:05:33 +0200

    updated email

--- api/Modules/Authentication/routes/web.php
+++ api/Modules/Authentication/routes/web.php
@@ -7,8 +7,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('authentication', AuthenticationController::class)->names('authentication');
 });
 
-Route::get('/setup-password/{token}', [AuthenticationController::class, 'showPasswordSetupForm'])
-    ->name('auth.password.setup.form');
+Route::prefix('api')->group(function () {
+    Route::get('/setup-password/{token}', [AuthenticationController::class, 'showPasswordSetupForm'])
+        ->name('auth.password.setup.form');
 
-Route::post('/setup-password/{token}', [AuthenticationController::class, 'completePasswordSetup'])
-    ->name('auth.password.setup.complete');
+    Route::post('/setup-password/{token}', [AuthenticationController::class, 'completePasswordSetup'])
+        ->name('auth.password.setup.complete');
+});
